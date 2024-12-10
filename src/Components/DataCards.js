026f9ed1810295f54a3fae04c36b4c66b0930ac6
unknown <hashmiabdulmajid@gmail.com>
2024-12-10 21:19:36 +0530

    updated images

--- src/Components/DataCards.js
+++ src/Components/DataCards.js
@@ -11,7 +11,7 @@ const DataCards = () => {
             <h1>Depression </h1>
           </div>
           <div>
-            <img className="disease-img" src="https://www.pngall.com/wp-content/uploads/15/Depression-Background-PNG.png" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/011/411/660/non_2x/man-felling-depression-under-moral-stressful-sad-feel-guilty-need-attention-help-sitting-alone-vector.jpg" />
           </div>
         </div>
         <div className="disease">
@@ -19,7 +19,7 @@ const DataCards = () => {
             <h1>Bipolar Disorder</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/025/677/823/non_2x/woman-with-bipolar-disorder-symptom-in-flat-design-bipolar-patient-with-mood-swings-sometimes-in-a-good-mood-sometimes-sad-vector.jpg" />
           </div>
         </div>
         <div className="disease">
@@ -27,7 +27,7 @@ const DataCards = () => {
             <h1>Seasonal Affective Disorder</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/003/685/842/non_2x/seasonal-affective-disorder-color-icon-mood-swing-emotional-change-manic-and-depressive-episodes-anxiety-low-energy-mental-health-psychological-problem-isolated-illustration-vector.jpg" />
           </div>
         </div>
         <div className="disease">
@@ -35,7 +35,7 @@ const DataCards = () => {
             <h1>Generalized Anxiety Disorder</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/003/686/296/non_2x/anxiety-disorder-color-icon-fear-and-worry-depressed-man-panic-attack-distress-headache-and-migraine-confused-thoughts-mental-problem-stress-and-tension-isolated-illustration-vector.jpg" />
           </div>
         </div>
         <div className="disease">
@@ -45,7 +45,7 @@ const DataCards = () => {
           <div>
             <img
               className="disease-img"
-              src="https://superblog.supercdn.cloud/site_cuid_cl92i00jg261301kozfglx818f/images/schizophrenia-cover-page-1685539381732-compressed.jpg"
+              src="https://static.vecteezy.com/system/resources/previews/007/820/657/non_2x/woman-with-panic-attack-anxiety-disorder-symptoms-vector.jpg"
             />
           </div>
         </div>
@@ -54,7 +54,7 @@ const DataCards = () => {
             <h1>Social Anxiety Disorder</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/011/415/865/non_2x/social-anxiety-disorder-concept-icon-mental-issues-downside-of-social-media-abstract-idea-thin-line-illustration-isolated-outline-drawing-editable-stroke-vector.jpg" />
           </div>
         </div>
         <div className="disease">
@@ -62,7 +62,7 @@ const DataCards = () => {
             <h1>Obsessive-Compulsive Disorder</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/008/552/155/non_2x/little-girl-with-obsessive-compulsive-disorder-children-s-mental-health-illustration-in-flat-style-vector.jpg" />
           </div>
         </div>
         <div className="disease">
@@ -70,7 +70,7 @@ const DataCards = () => {
             <h1>Post-Traumatic Stress Disorder</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/006/031/631/non_2x/post-traumatic-stress-disorder-shocked-scared-shock-panic-surprise-face-angry-and-frustrated-fear-and-upset-for-mistake-concept-illustration-free-vector.jpg" />
           </div>
         </div>
 
@@ -79,7 +79,7 @@ const DataCards = () => {
             <h1>Phobias</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/030/724/196/non_2x/man-suffers-from-phobias-and-fears-the-psychological-concept-of-mental-disorder-and-paranoia-illustration-vector.jpg" />
           </div>
         </div>
          <div className="disease">
@@ -87,7 +87,7 @@ const DataCards = () => {
             <h1>Attention Deficit Hyperactivity Disorder</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/047/646/939/non_2x/attention-deficit-hyperactivity-disorder-behavior-illustration-vector.jpg" />
           </div>
         </div>
         <div className="disease">
@@ -95,7 +95,7 @@ const DataCards = () => {
             <h1>Autism Spectrum Disorder </h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/050/526/249/non_2x/asperger-syndrome-concept-illustration-design-vector.jpg" />
           </div>
         </div>
         <div className="disease">
@@ -103,7 +103,7 @@ const DataCards = () => {
             <h1>Learning Disorders</h1>
           </div>
           <div>
-            <img className="disease-img" src="" />
+            <img className="disease-img" src="https://static.vecteezy.com/system/resources/previews/007/718/681/non_2x/dyslexia-failing-to-read-learning-disability-concept-young-girl-difficulty-in-reading-logopedy-scattered-letters-above-her-head-and-book-illustration-vector.jpg" />
           </div>
         </div>
       </div>
