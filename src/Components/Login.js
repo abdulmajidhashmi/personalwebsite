d1ed281d7a9846058ac811700bba9620b94f296f
unknown <hashmiabdulmajid@gmail.com>
2024-12-12 19:40:01 +0530

    login css updated

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,16 +5,19 @@ const Login = () => {
     <>
       <div className="login-maindiv">
         <div className="login-div">
-          <h1>Login here</h1>
-
+          <h1 className="l-head">Login here</h1>
+          <Link to='/'><i class="fa-solid fa-xmark cross-mark"></i></Link>
+<div className="inp-cont">
           <input className="login-inp" placeholder="Enter Email" />
           <input className="login-inp" placeholder="Enter password" />
 
           <div className="login-click">Login</div>
 
-          <Link to="/signup">
-            <p>New user?Sign up here</p>
+         <Link to="/signup">
+        <div className="signuphere-div"><p>New user?</p> <p className="signup-here">Sign up here</p></div> 
           </Link>
+
+          </div>
         </div>
       </div>
     </>
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -7,14 +7,18 @@ const Signup = ()=>{
         <div className='signup-maindiv'>
         <div className='signup-div'>
         
-        <h1>Signup here</h1>
+        <h1 className='s-head'>Sign up here</h1>
+
+        <div className='s-inp-div'>
+       <Link to="/"><i class="fa-solid fa-xmark cross-mark"></i></Link> 
 
         <input className='signup-inp' placeholder='Enter Name'/>
         <input className='signup-inp' placeholder='Enter Email'/>
         <input className='signup-inp' type='password' placeholder='Enter Password'/>
         <input className='signup-inp'type='password' placeholder='Confirm your password'/>
         <div className='signup-button'>Sign Up</div>
-        <div className='already-userdiv'><p>Already a user?</p><Link to="/login"><p className='already-user'>Login here</p></Link></div>
+        <Link to="/login"><div className='already-userdiv'><p>Already a user?</p><p className='already-user'>Login here</p></div></Link>
+        </div>
         </div>
         </div>
     )
