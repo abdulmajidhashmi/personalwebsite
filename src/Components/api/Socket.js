c07d7ba0b51f9c49a989e3ad31d6eabdef62e012
unknown <hashmiabdulmajid@gmail.com>
2024-12-19 11:56:35 +0530

    server adress changed

--- src/Components/api/Socket.js
+++ src/Components/api/Socket.js
@@ -1,8 +1,8 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-// const SOCKET_URL = "https://personalwebsitebackend-ntzy.onrender.com";
-const SOCKET_URL = 'http://localhost:4020' // Replace with your backend URL
+ const SOCKET_URL = "https://personalwebsitebackend-ntzy.onrender.com";
+//const SOCKET_URL = 'http://localhost:4020' // Replace with your backend URL
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket"], // Ensure WebSocket transport
--- src/Components/api/axiosInstance.js
+++ src/Components/api/axiosInstance.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const axiosInstance =axios.create({
 
-    baseURL:"http://localhost:4020",
-    //baseURL:"https://personalwebsitebackend-ntzy.onrender.com"
+    // baseURL:"http://localhost:4020",
+    baseURL:"https://personalwebsitebackend-ntzy.onrender.com"
 })
 
 export default axiosInstance;
