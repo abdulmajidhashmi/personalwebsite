b07f492093184b189c782ba9be207473f2f49090
unknown <hashmiabdulmajid@gmail.com>
2024-12-20 12:47:21 +0530

    useffect

--- src/Components/SingleUser.js
+++ src/Components/SingleUser.js
@@ -116,7 +116,7 @@ console.log(deliveredRef);
     
     };
   
-  }, [local.number,use,deliveredRef]);
+  }, [local.number,use,isRefsready]);
   const changeMessage = (event) => {
     const val = event.target.value;
     setmsg(val);
--- src/Components/Test.js
+++ src/Components/Test.js
@@ -105,7 +105,7 @@ const Test = () => {
         }
       };
     }
-  }, [local.number]);
+  }, [local.number,isRefsready]);
 
  
 
