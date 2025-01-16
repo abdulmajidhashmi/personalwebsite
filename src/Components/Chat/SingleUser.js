da9c7e1af011e362c6c70cfa394d07760526c844
unknown <hashmiabdulmajid@gmail.com>
2025-01-16 18:46:25 +0530

    video call changes

--- src/Components/Chat/SingleUser.js
+++ src/Components/Chat/SingleUser.js
@@ -1,6 +1,6 @@
 import "./Test.css";
 import { useRef, useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Alert, Flex, Spin } from 'antd';
 import axiosInstance from "../api/axiosInstance";
 import { io } from "socket.io-client";
@@ -152,7 +152,7 @@ const SingleUser = () => {
             </div>
           </div>
           <div class="header-actions">
-            <button class="action-button">
+          <Link to="/videoCall"> <button class="action-button">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="icon"
@@ -167,7 +167,7 @@ const SingleUser = () => {
                   d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
                 />
               </svg>
-            </button>
+            </button></Link>
             <button class="action-button">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
