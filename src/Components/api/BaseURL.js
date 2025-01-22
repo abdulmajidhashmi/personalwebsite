a80181e12558ceacb1d45c24260406cf73316a3a
unknown <hashmiabdulmajid@gmail.com>
2025-01-22 18:52:22 +0530

    changes azure

--- src/Components/api/BaseURL.js
+++ src/Components/api/BaseURL.js
@@ -1,6 +1,5 @@
-const baseURL = "http://74.225.174.247:3000";
+const baseURL = "http://localhost:4020";
 // const baseURL = 'https://personalwebsitebackend-ntzy.onrender.com';
-//const baseURL = 'https://0.0.0.0:80';
 
 
 export default baseURL;
