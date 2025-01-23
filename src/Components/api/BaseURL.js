caba8e82b60129125890f06eaec9f26ea4e4a08a
unknown <hashmiabdulmajid@gmail.com>
2025-01-23 21:28:44 +0530

    ll

--- src/Components/api/BaseURL.js
+++ src/Components/api/BaseURL.js
@@ -1,5 +1,5 @@
-const baseURL = "http://localhost:4020";
+//const baseURL = "http://localhost:4020";
 // const baseURL = 'https://personalwebsitebackend-ntzy.onrender.com';
-
+const baseURL = "http://4.213.176.68:4020";
 
 export default baseURL;
